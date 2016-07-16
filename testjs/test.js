e05d133789ddda1836896bc1e4c2204e26135dee
Clumsycat <31643705@qq.com>
2016-07-16 16:49:35 +0800

    add travis.yml“

--- testjs/test.js
+++ testjs/test.js
@@ -3,8 +3,9 @@ describe("first test", function(){
 		  var store = {};
 	});
 		
-	it("save the cartlist",function(){
+	it("init the cartlist",function(){
 		doInit();
 		expect(localStorage.getItem("cartlist")).toEqual('{"ITEM000001":5,"ITEM000002":1,"ITEM000003":2,"ITEM000005":3}');
 	});
+
 });
