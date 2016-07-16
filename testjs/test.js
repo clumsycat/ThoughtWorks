49bc4ad4012ae5a9a0bea12fb3e98d43595c1589
Clumsycat <31643705@qq.com>
2016-07-16 16:36:31 +0800

    add an easy test

---
+++ testjs/test.js
@@ -0,0 +1,10 @@
+describe("first test", function(){
+	beforeEach(function(){
+		  var store = {};
+	});
+		
+	it("save the cartlist",function(){
+		doInit();
+		expect(localStorage.getItem("cartlist")).toEqual('{"ITEM000001":5,"ITEM000002":1,"ITEM000003":2,"ITEM000005":3}');
+	});
+});
